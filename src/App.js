225d0a09dbf62a6af1adaae5662659300204130e
Navdeep <nav@Navdeeps-Air.hitronhub.home>
2022-10-08 16:41:47 -0400

    fix navigation issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import AppLayout from 'components/layout/AppLayout';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import AppLayout from './components/layout/AppLayout';
 import PageNotFound from './pages/404';
 import AllHabits from './pages/all-habits';
 import Dashboard from './pages/dashboard';
 import Login from './pages/login';
 import Today from './pages/today';
 import { auth } from './services';
-
 function App() {
 	const [user, setUser] = useState(null);
 
@@ -17,21 +16,21 @@ function App() {
 	}, []);
 
 	if (user == null) return 'loading...';
-
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route
+					path='/'
 					element={
-						!user ? (
-							<Navigate to='/login' />
-						) : (
+						user ? (
 							<AppLayout user={user} />
+						) : (
+							<Navigate to='/login' />
 						)
 					}>
-					<Route path='/dashboard' element={<Dashboard />} />
-					<Route path='/today' element={<Today />} />
-					<Route path='/all-habits' element={<AllHabits />} />
+					<Route path='dashboard' element={<Dashboard />} />
+					<Route path='today' element={<Today />} />
+					<Route path='all-habits' element={<AllHabits />} />
 				</Route>
 				<Route path='/login' element={<Login user={user} />} />
 				<Route path='*' element={<PageNotFound />} />
