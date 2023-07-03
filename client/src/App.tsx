552a2edb06ae35293dd6062bfd8d5c1ed0cf5e60
Navdeep <nsdhammu@gmail.com>
2023-07-03 15:19:38 -0400

    fix component folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,42 @@
-import AppLayout from 'components/layout/AppLayout'
-import { Route, Switch } from 'wouter'
-import PageNotFound from 'pages/404'
-import Dashboard from 'pages/dashboard/index.tsx'
-import LoginPage from 'pages/login'
-import Today from 'pages/today'
-import PrivateRoute from './PrivateRoute'
-import PublicRoute from './PublicRoute'
-import AuthProvider from 'context/AuthContext.tsx'
-import HabitsProvider from 'context/HabitsContext.tsx'
-import AllHabitsPage from 'pages/all-habits'
+import AppLayout from 'components/AppLayout';
+import { Route, Switch } from 'wouter';
+import PageNotFound from 'pages/404';
+import Dashboard from 'pages/dashboard/index.tsx';
+import LoginPage from 'pages/login';
+import Today from 'pages/today';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+import AuthProvider from 'context/AuthContext.tsx';
+import HabitsProvider from 'context/HabitsContext.tsx';
+import AllHabitsPage from 'pages/all-habits';
 
 function App() {
-    return (
-        <AuthProvider>
-            <HabitsProvider>
-                <Switch>
-                    <PublicRoute path="/">
-                        <LoginPage />
-                    </PublicRoute>
-                    <PublicRoute path="/login">
-                        <LoginPage />
-                    </PublicRoute>
-                    <PrivateRoute path="/app/dashboard">
-                        <AppLayout view={<Dashboard />} />
-                    </PrivateRoute>
-                    <PrivateRoute path="/app/today">
-                        <AppLayout view={<Today />} />
-                    </PrivateRoute>
-                    <PrivateRoute path="/app/habits">
-                        <AppLayout view={<AllHabitsPage />} />
-                    </PrivateRoute>
-                    <Route>
-                        <PageNotFound />
-                    </Route>
-                </Switch>
-            </HabitsProvider>
-        </AuthProvider>
-    )
+   return (
+      <AuthProvider>
+         <HabitsProvider>
+            <Switch>
+               <PublicRoute path="/">
+                  <LoginPage />
+               </PublicRoute>
+               <PublicRoute path="/login">
+                  <LoginPage />
+               </PublicRoute>
+               <PrivateRoute path="/app/dashboard">
+                  <AppLayout view={<Dashboard />} />
+               </PrivateRoute>
+               <PrivateRoute path="/app/today">
+                  <AppLayout view={<Today />} />
+               </PrivateRoute>
+               <PrivateRoute path="/app/habits">
+                  <AppLayout view={<AllHabitsPage />} />
+               </PrivateRoute>
+               <Route>
+                  <PageNotFound />
+               </Route>
+            </Switch>
+         </HabitsProvider>
+      </AuthProvider>
+   );
 }
 
-export default App
+export default App;
---
+++ client/src/components/AppLayout.tsx
@@ -0,0 +1,16 @@
+import { Box, Flex } from '@chakra-ui/react';
+import { ReactElement } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+export default function AppLayout({ view }: { view: ReactElement }) {
+   return (
+      <Flex h="100vh" direction={['column', null, 'row']}>
+         <Sidebar />
+         <Header />
+         <Box as="main" flex="1" overflowY="scroll" p={[2, 6, 8]} bg="gray.50">
+            {view}
+         </Box>
+      </Flex>
+   );
+}
