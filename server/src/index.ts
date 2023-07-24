56d5c614142655f57bb7a90ed0a1d151ce6eed35
Navdeep <nsdhammu@gmail.com>
2023-07-24 17:20:14 -0400

    troubleshoot deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -63,7 +63,7 @@ app.register(authRoutes, { prefix: '/api' });
 app.register(habitsRoutes, { prefix: '/api' });
 
 app.listen(
-   { ipv6Only: true, port: (process.env.PORT as unknown as number) || 3000 },
+   { port: (process.env.PORT as unknown as number) || 3000 },
    function (err, address) {
       if (err) {
          app.log.error(err);
