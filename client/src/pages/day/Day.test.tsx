bbcab6296655ec605ac99def79d382e0322d5408
Navdeep <nsdhammu@gmail.com>
2024-07-20 18:00:00 +0200

    test commit jul22158

---
+++ client/src/pages/day/Day.test.tsx
@@ -0,0 +1,9 @@
+import { render } from 'test-utils'
+import Day from "."
+
+describe('Day', () => {
+        test('should have welcome message with users name', () => {
+                render(<Day />)
+                
+        })
+})
